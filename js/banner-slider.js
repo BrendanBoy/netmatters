aa3e54310ca850eed9bc6d0e395989baebd627c0
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2022-01-07 14:22:20 +0000

    Add the HTML content for all of the banner slides

--- js/banner-slider.js
+++ js/banner-slider.js
@@ -2,7 +2,7 @@ var slider = tns({
     container: '.my-slider',
     items: 1,
     slideBy: 'page',
-    autoplay: true,
+    // autoplay: true,
     controls: false,
     navPosition: "bottom",
     autoplayButtonOutput: false,
