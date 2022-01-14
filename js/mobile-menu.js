8be9d8b6314795628ac1d00bb134b11e1b941148
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2022-01-14 23:54:15 +0000

    Attempt to create an overlay for the main content while mobile nav is active

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,11 +1,22 @@
+const mobileMenu = document.getElementById("mobileMenu");
+const mainContent = document.getElementById("mainContent");
+const body = document.getElementById("body");
+const mainContentBefore = window.getComputedStyle(mainContent, "::before");
+
 function openNav() {
-    document.getElementById("mobileMenu").classList.add("menu-active");
-    document.getElementById("mainContent").classList.add("inactive");
-    document.getElementById("body").style.overflow = "hidden";
+    mobileMenu.classList.add("menu-active");
+    mainContent.classList.add("inactive");
+    body.style.overflow = "hidden";
+    mainContent.style.setProperty("--overlay", "block");
+    mainContent.style.setProperty("--zed", "4");
+    mainContent.style.setProperty("--opaque", "0.5");
 }
 
 function closeNav() {
-    document.getElementById("mobileMenu").classList.remove("menu-active");
-    document.getElementById('mainContent').classList.remove("inactive");
-    document.getElementById("body").style.overflow = "auto";
+    mobileMenu.classList.remove("menu-active");
+    mainContent.classList.remove("inactive");
+    body.style.overflow = "auto";
+    mainContent.style.setProperty("--overlay", "none");
+    mainContent.style.setProperty("--zed", "0");
+    mainContent.style.setProperty("--opaque", "0");
 }
