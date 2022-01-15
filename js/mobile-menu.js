6c4df8074602e2ae1756720ddd922b98554de192
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2022-01-15 00:42:20 +0000

    Create an overlay for the main content while the mobile nav is active, that actually works as expected

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,22 +1,22 @@
 const mobileMenu = document.getElementById("mobileMenu");
 const mainContent = document.getElementById("mainContent");
 const body = document.getElementById("body");
-const mainContentBefore = window.getComputedStyle(mainContent, "::before");
+const mainContentOverlay = document.getElementById("mainContentOverlay");
 
 function openNav() {
     mobileMenu.classList.add("menu-active");
     mainContent.classList.add("inactive");
+    mainContentOverlay.classList.add("inactive");
     body.style.overflow = "hidden";
-    mainContent.style.setProperty("--overlay", "block");
-    mainContent.style.setProperty("--zed", "4");
-    mainContent.style.setProperty("--opaque", "0.5");
+    mainContentOverlay.style.width = "100%";
+    mainContentOverlay.style.opacity = "0.8";
 }
 
 function closeNav() {
     mobileMenu.classList.remove("menu-active");
     mainContent.classList.remove("inactive");
+    mainContentOverlay.classList.remove("inactive");
     body.style.overflow = "auto";
-    mainContent.style.setProperty("--overlay", "none");
-    mainContent.style.setProperty("--zed", "0");
-    mainContent.style.setProperty("--opaque", "0");
+    mainContentOverlay.style.width = "0%";
+    mainContentOverlay.style.opacity = "0";
 }
