f4eb931ccbe8590c4716447b96987fc904bd94dd
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2022-01-20 11:35:23 +0000

    Fix Sticky Header

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,82 +1,50 @@
-// The new version that still doesn't work right
+// I think quite a lot of this can be trimmed out at some point
+// but right now I'm just happy that it works
 const header = document.getElementById("header");
 const scrollUp = "scroll-up";
 const scrollDown = "scroll-down";
 let lastScroll = 0;
 
+function headerCheck() {
+    const pageWidth = window.innerWidth;
+    if (pageWidth < 768) {
+        let headerHeight = 168;
+        console.log("Header Height is ", headerHeight);
+        return headerHeight;
+    } else if (pageWidth >= 768 && pageWidth < 992) {
+        let headerHeight = 110;
+        console.log("Header Height is ", headerHeight);
+        return headerHeight;
+    } else if (pageWidth >= 992) {
+        let headerHeight = 224;
+        console.log("Header Height is ", headerHeight);
+        return headerHeight;
+    }
+    console.log("Page Width is ", pageWidth);
+}
+
 window.addEventListener("scroll", () => {
     const currentScroll = window.scrollY;
     console.log("Scroll Position is ", currentScroll);
+    let headerHeight = headerCheck();
 
     if (currentScroll <= 0) {
         header.classList.remove(scrollUp);
+        header.style.position = "static";
         return;
     }
-    if (currentScroll < 224) {
-        header.style.position = "static";
-    }
-    if (currentScroll >= 224) {
-        header.style.position = "sticky";
-    }
-    if (currentScroll > 224 && currentScroll > lastScroll && !header.classList.contains(scrollDown)) {
+    if (currentScroll > headerHeight && currentScroll > lastScroll && !header.classList.contains(scrollDown)) {
         // down
         header.classList.remove(scrollUp);
         header.classList.add(scrollDown);
+        
         console.log("Scrolling downwards");
     } else if (currentScroll < lastScroll && header.classList.contains(scrollDown)) {
         // up
         header.classList.remove(scrollDown);
         header.classList.add(scrollUp);
+        header.style.position = "sticky";
         console.log("Scrolling upwards");
     }
     lastScroll = currentScroll;
 });
-
-// /////////////////
-// The Old Version
-// /////////////////
-// const header = document.getElementById("header");
-// const scrollUp = "scroll-up";
-// const scrollDown = "scroll-down";
-// let lastScroll = 0;
-
-// function headerCheck() {
-//     const pageWidth = window.innerWidth;
-//     if (pageWidth < 768) {
-//         let headerHeight = 168;
-//         console.log("Header Height is ", headerHeight);
-//         return headerHeight;
-//     } else if (pageWidth >= 768 && pageWidth < 992) {
-//         let headerHeight = 110;
-//         console.log("Header Height is ", headerHeight);
-//         return headerHeight;
-//     } else if (pageWidth >= 992) {
-//         let headerHeight = 224;
-//         console.log("Header Height is ", headerHeight);
-//         return headerHeight;
-//     }
-//     console.log("Page Width is ", pageWidth);
-// }
-
-// window.addEventListener("scroll", () => {
-//     const currentScroll = window.scrollY;
-//     console.log("Scroll Position is ", currentScroll);
-//     let headerHeight = headerCheck();
-
-//     if (currentScroll <= 0) {
-//         header.classList.remove(scrollUp);
-//         return;
-//     }
-//     if (currentScroll > headerHeight && currentScroll > lastScroll && !header.classList.contains(scrollDown)) {
-//         // down
-//         header.classList.remove(scrollUp);
-//         header.classList.add(scrollDown);
-//         console.log("Scrolling downwards");
-//     } else if (currentScroll < lastScroll && header.classList.contains(scrollDown)) {
-//         // up
-//         header.classList.remove(scrollDown);
-//         header.classList.add(scrollUp);
-//         console.log("Scrolling upwards");
-//     }
-//     lastScroll = currentScroll;
-// });
