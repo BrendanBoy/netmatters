559bf3fa6df3d7f0dac3746ce6579022c73b8844
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2022-03-24 15:19:08 +0000

    Move the header and footer sections into reusable files

---
+++ inc/footer.php
@@ -0,0 +1,189 @@
+        </main>
+            <section id="newsletter">
+                <h2 class="sr-only">Newsletter</h2>
+                <div class="container">
+                    <fieldset class="p-5">
+                        <legend>Email Newsletter Sign-Up</legend>
+                        <form>
+                            <div class="row mb-3">
+                                <div class="form-group col-lg-6">
+                                    <label for="inputName" class="form-label">Your Name <span class="asterisk">*</span></label>
+                                    <input type="text" class="form-control" id="inputName">
+                                </div>
+                                <div class="form-group col-lg-6">
+                                    <label for="inputEmail" class="form-label">Your Email <span class="asterisk">*</span></label>
+                                    <input type="email" class="form-control" id="inputEmail">
+                                </div>
+                            </div>
+                            <div class="form-check ms-2 mb-3">
+                                <input type="checkbox" class="form-check-input" value="" id="marketing">
+                                <label for="marketing" class="form-check-label">Please tick this box if you want to receive
+                                    marketing information from us. Please see our <a href="#">Privacy Policy</a> for more
+                                    information on how we use your data.</label>
+                            </div>
+                            <button type="submit" class="btn btn-altDark">Subscribe</button>
+                        </form>
+                    </fieldset>
+                </div>
+            </section>
+        <footer>
+            <div class="container">
+                <div class="feet">
+                    <div class="foot-navs">
+                        <div class="foot-nav">
+                            <h4>About Netmatters</h4>
+                            <ul class="nav flex-column">
+                                <li class="nav-item"><a href="#">News</a></li>
+                                <li class="nav-item"><a href="#">Our Careers</a></li>
+                                <li class="nav-item"><a href="#">Our Team</a></li>
+                                <li class="nav-item"><a href="#">Our Office Tour</a></li>
+                                <li class="nav-item"><a href="#">Privacy Policy</a></li>
+                                <li class="nav-item"><a href="#">Cookie Policy</a></li>
+                                <li class="nav-item"><a href="#">Terms & Conditions</a></li>
+                                <li class="nav-item"><a href="#">Environmental Policy</a></li>
+                            </ul>
+                        </div>
+                        <div class="foot-nav">
+                            <h4>Our Services</h4>
+                            <ul class="nav flex-column">
+                                <li class="nav-item"><a href="#">Bespoke Software</a></li>
+                                <li class="nav-item"><a href="#">IT Support</a></li>
+                                <li class="nav-item"><a href="#">Digital Marketing</a></li>
+                                <li class="nav-item"><a href="#">Telecoms Services</a></li>
+                                <li class="nav-item"><a href="#">Web Design</a></li>
+                                <li class="nav-item"><a href="#">Cyber Security</a></li>
+                                <li class="nav-item"><a href="#">Developer Training</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="offices">
+                        <div class="office">
+                            <h4>Cambridge Office</h4>
+                            <address>
+                                Unit 1.28,<br>
+                                St John's Innovation Centre,<br>
+                                Cowley Road,<br>
+                                Cambridge,<br>
+                                CB4 0WS<br>
+                                <br>
+                                Tel: <a href="#">01223 37 57 72</a>
+                            </address>
+                        </div>
+                        <div class="office">
+                            <h4>Wymondham Office</h4>
+                            <address>
+                                Unit 15,<br>
+                                Penfold Drive,<br>
+                                Gateway 11 Business Park,<br>
+                                Wymondham, Norfolk,<br>
+                                NR18 0WZ<br>
+                                <br>
+                                Tel: <a href="#">01603 70 40 20</a>
+                            </address>
+                        </div>
+                        <div class="office">
+                            <h4>Great Yarmouth Office</h4>
+                            <address>
+                                Suite F23,<br>
+                                Beacon Innovation Centre,<br>
+                                Beacon Park, Gorleston,<br>
+                                Great Yarmouth, Norfolk,<br>
+                                NR31 7RA<br>
+                                <br>
+                                Tel: <a href="#">01493 60 32 04</a>
+                            </address>
+                        </div>
+                    </div>
+                </div>
+                <hr class="foot-rule">
+                <div class="outro">
+                    <div>&copy; Copyright Netmatters Ltd. 2021. All rights reserved.</div>
+                    <span class="mx-2"> - </span>
+                    <div><a href="#">Sitemap</a></div>
+                    <div class="socials">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <section id="awards">
+            <h2 class="sr-only">Awards and Accreditations</h2>
+            <div class="container d-none d-md-block">
+                <div class="d-flex justify-content-around my-5">
+                    <div class="mx-2">
+                        <a href="#"><img src="img/awards/PartnerBadge.png" class="img-fluid" alt="Google Partner badge."></a>
+                    </div>
+                    <div class="mx-2">
+                        <img src="img/awards/silver-microsoft-partner.jpg" class="img-fluid" alt="Silver Microsoft Partner">
+                    </div>
+                    <div class="mx-2 colour-change">
+                        <img src="img/awards/future-50.jpg" class="img-fluid" alt="Future 50 Member">
+                        <img src="img/awards/future-50_greyscale.jpeg" class="greyscale img-fluid" alt="Future 50 Member, grey">
+                    </div>
+                    <div class="mx-2 colour-change">
+                        <img src="img/awards/qms.jpg" class="img-fluid" alt="QMS">
+                        <img src="img/awards/qms_greyscale.jpeg" class="greyscale img-fluid" alt="QMS, grey">
+                    </div>
+                    <div class="mx-2 colour-change">
+                        <img src="img/awards/norfolk-carbon-charter.jpg" class="img-fluid" alt="Norfolk Carbon Charter">
+                        <img src="img/awards/norfolk-carbon-charter_greyscale.jpeg" class="greyscale img-fluid" alt="Norfolk Carbon Charter, grey">
+                    </div>
+                </div>
+                <div class="d-flex justify-content-around mb-5">
+                    <div class="mx-2 colour-change">
+                        <img src="img/awards/investing-in-future-growth.jpg" class="img-fluid" alt="Investing in Future Growth">
+                        <img src="img/awards/investing-in-future-growth_greyscale.jpeg" class="greyscale img-fluid" alt="Investing in Future Growth, grey">
+                    </div>
+                    <div class="mx-2 colour-change">
+                        <img src="img/awards/princess-royal-training.jpg" class="img-fluid" alt="Princess Royal Training">
+                        <img src="img/awards/princess-royal-training_greyscale.jpeg" class="greyscale img-fluid" alt="Princess Royal Training, grey">
+                    </div>
+                    <div class="mx-2 colour-change">
+                        <img src="img/awards/skills-of-tomorrow.jpg" class="img-fluid" alt="Skills of Tomorrow">
+                        <img src="img/awards/skills-of-tomorrow_greyscale.jpeg" class="greyscale img-fluid" alt="Skills of Tomorrow, grey">
+                    </div>
+                    <div class="mx-2 colour-change">
+                        <a href="#">
+                            <img src="img/awards/PPC_logo.jpg" class="img-fluid" alt="PPC logo">
+                            <img src="img/awards/PPC_logo_greyscale.jpeg" class="greyscale img-fluid" alt="PPC logo, grey">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div id="modal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title">Cookies Policy</h3>
+            </div>
+            <div class="modal-body">
+                <p>Our website uses cookies. This helps us provide you with a good experience on our website. To see what cookies we use and what they do, and to opt-in on non-essential cookies click "change settings". For a detailed explanation, click on "<a href="#">Privacy Policy</a>" otherwise click "Accept Cookies" to enter.</p>
+            </div>
+            <div class="modal-footer">
+                <div class="row">
+                    <div class="col-md-6 spacer">
+                        <a class="btn btn-altLight settings" href="#">Change Settings</a>
+                    </div>
+                    <div class="col-md-6">
+                        <a class="btn btn-design accept" href="#">Accept Cookies</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
+        crossorigin="anonymous">
+    </script>
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/banner-slider.js"></script>
+    <script src="js/mobile-menu.js"></script>
+    <script src="js/sticky-header.js"></script>
+    <script src="js/pop-up.js"></script>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/netmatters-ltd-VXAv.png">
+    <title>Netmatters | Full Service Digital Agency | Norwich, Norfolk | Netmatters</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700;800&display=swap" rel="stylesheet"> 
+    <script src="https://kit.fontawesome.com/3f8b63cd88.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/testing.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/owl.theme.default.css">
+</head>
+
+<body id="body">
+    <section id="mobileMenu" class="mobile-menu" onclick="closeNav()">
+        <div class="main-nav-mobile">
+            <ul>
+                <li>
+                    <a href="#">Contact Us</a>
+                </li>
+                <li>
+                    <a class="drop-btn design-btn" href="#">
+                        <div class="icon"><i class="bi bi-code-slash"></i></div>
+                        <div class="small">Web</div>
+                        <div>Design</div>
+                    </a>
+                    <ul>
+                        <li><a href="#">Stylish Websites</a></li>
+                        <li><a href="#">Ecommerce Stores</a></li>
+                        <li><a href="#">Branding</a></li>
+                        <li><a href="#">Apps</a></li>
+                        <li><a href="#">Web Hosting</a></li>
+                        <li><a href="#">Pay Monthly Websites</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="drop-btn support-btn" href="#">
+                        <div class="icon"><i class="bi bi-display"></i></div>
+                        <div class="small">It</div>
+                        <div>Support</div>
+                    </a>
+                    <ul>
+                        <li><a href="#">Managed It</a></li>
+                        <li><a href="#">Business It</a></li>
+                        <li><a href="#">Office 365</a></li>
+                        <li><a href="#">Consultancy</a></li>
+                        <li><a href="#">Cloud Provider</a></li>
+                        <li><a href="#">Data Backup</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="drop-btn telecoms-btn" href="#">
+                        <div class="icon"><i class="fas fa-phone-alt"></i></div>
+                        <div class="small">Telecoms</div>
+                        <div>Services</div>
+                    </a>
+                    <ul>
+                        <li><a href="#">Business Mobile</a></li>
+                        <li><a href="#">Hosted Voip</a></li>
+                        <li><a href="#">Business Voip</a></li>
+                        <li><a href="#">Business Broadband</a></li>
+                        <li><a href="#">Leased Line</a></li>
+                        <li><a href="#">3CX Systems</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="drop-btn software-btn" href="#">
+                        <div class="icon"><i class="fas fa-th"></i></div>
+                        <div class="small">Bespoke</div>
+                        <div>Software</div>
+                    </a>
+                    <ul>
+                        <li><a href="#">Workflow</a></li>
+                        <li><a href="#">Automation</a></li>
+                        <li><a href="#">Integration</a></li>
+                        <li><a href="#">Database</a></li>
+                        <li><a href="#">Sharepoint</a></li>
+                        <li><a href="#">Management</a></li>
+                        <li><a href="#">Navision</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="drop-btn marketing-btn" href="#">
+                        <div class="icon"><i class="bi bi-bar-chart-fill"></i></div>
+                        <div class="small">Digital</div>
+                        <div>Marketing</div>
+                    </a>
+                    <ul>
+                        <li><a href="#">Search (SEO)</a></li>
+                        <li><a href="#">Paid (PPC)</a></li>
+                        <li><a href="#">Conversion (CRO)</a></li>
+                        <li><a href="#">Email</a></li>
+                        <li><a href="#">Social Media</a></li>
+                        <li><a href="#">Content</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="drop-btn security-btn" href="#">
+                        <div class="icon"><i class="bi bi-shield-shaded"></i></div>
+                        <div class="small">Cyber</div>
+                        <div>Security</div>
+                    </a>
+                    <ul>
+                        <li><a href="#">Assesment</a></li>
+                        <li><a href="#">Management</a></li>
+                        <li><a href="#">Penetration Testing</a></li>
+                        <li><a href="#">Cyber Essentials</a></li>
+                        <li><a href="#">PCI/DSS</a></li>
+                        <li><a href="#">Hacker Prevention</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <ul>
+            <li>
+                <a href="#">Training</a>
+                <ul>
+                    <li><a href="#">Train For A Career In Tech</a></li>
+                    <li><a href="#">SCS Frequently Asked Question</a></li>
+                    <li><a href="#">Scion Collaborators</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Events</a>
+                <ul>
+                    <li><a href="#">Business Automation Seminar</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Our Company</a>
+                <ul>
+                    <li><a href="#">Our Culture</a></li>
+                    <li><a href="#">Our Team</a></li>
+                    <li><a href="#">Our Careers</a></li>
+                    <li><a href="#">Our Benefits</a></li>
+                    <li><a href="#">IT Support Great Yarmouth</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Our Work</a>
+                <ul>
+                    <li><a href="#">Case Studies</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Our Knowledge</a>
+                <ul>
+                    <li><a href="#">Guides</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Insights</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Covid Risk Assesments</a>
+            </li>
+            <li>
+                <a href="#">Contact Us</a>
+                <ul>
+                    <li><a href="#">Cambridge Office</a></li>
+                    <li><a href="#">Wymondham Office</a></li>
+                    <li><a href="#">Great Yarmouth Office</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Support</a>
+            </li>
+        </ul>
+    </section>
+    <div id="mainContent">
+        <div id="mainContentOverlay" class="overlay" onclick="closeNav()"></div>
+        <header id="header">
+            <div class="header-bar">
+                <div class="container py-4">
+                    <div class="top">
+                        <div class="net-logo">
+                            <a href="#">
+                                <img class="img-fluid" src="img/f-logo.png" alt="Netmatters logo">
+                            </a>
+                        </div>
+                        <div class="telephone d-md-none">
+                            <a href="#"><i class="fas fa-phone-alt"></i></a>
+                        </div>
+                        <div class="top-buttons">
+                            <a href="#" class="btn btn-top btn-support d-none d-lg-block">
+                                <div class="btn-inner">
+                                    <i class="fas fa-mouse"></i>
+                                    Support
+                                </div>
+                            </a>
+                            <a href="#" class="btn btn-top btn-altLight d-none d-lg-block">
+                                <div class="btn-inner">
+                                    <i class="fas fa-paper-plane"></i>
+                                    Contact
+                                </div>
+                            </a>
+                            <a href="#" class="btn btn-top btn-altLight top-search d-none d-lg-block d-xl-none">
+                                <i class="fas fa-search"></i>
+                            </a>
+                            <form id="search-box" class="d-none d-md-block d-lg-none d-xl-block">
+                                <div class="input-group">
+                                    <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+                                    <button type="button" class="btn btn-top btn-altLight"><i class="fas fa-search"></i></button>
+                                </div>
+                            </form>
+                            <button id="mobile-burger" class="hamburger hamburger--spin btn btn-top btn-altDark" type="button" onclick="openNav()">
+                                <span class="hamburger-box">
+                                    <span class="hamburger-inner"></span>
+                                </span><br>
+                                <span class="title">Menu</span>
+                            </button>
+                        </div>
+                    </div>
+                    <form class="mobile-search d-md-none">
+                        <input type="search" class="search-field" placeholder="Search..." aria-label="Search">
+                        <button type="button" class="search-button"><i class="fas fa-search"></i></button>
+                    </form>
+                </div>
+            </div>
+
+            <!-- main site navigation -->
+            <nav id="main-nav">
+                <ul class="drop nav nav-justified container d-none d-lg-flex">
+                    
+                    <li class="design-box drop-box nav-item">
+                        <a class="design-btn drop-btn" href="#">
+                            <div class="icon"><i class="bi bi-code-slash"></i></div>
+                            <div class="small">Web</div>
+                            <div>Design</div>
+                        </a>
+                        <span class="back-bar design-bar"></span>
+                        <ul class="design-menu drop-menu">
+                            <li><a href="#">Stylish Websites</a></li>
+                            <li><a href="#">Ecommerce Stores</a></li>
+                            <li><a href="#">Branding</a></li>
+                            <li><a href="#">Apps</a></li>
+                            <li><a href="#">Web Hosting</a></li>
+                            <li><a href="#">Pay Monthly Websites</a></li>
+                        </ul>
+                    </li>
+                    
+                    <li class="support-box drop-box nav-item">
+                        <a class="support-btn drop-btn" href="#">
+                            <div class="icon"><i class="bi bi-display"></i></div>
+                            <div class="small">It</div>
+                            Support
+                        </a>
+                        <span class="back-bar support-bar"></span>
+                        <ul class="support-menu drop-menu">
+                            <li><a href="#">Managed It</a></li>
+                            <li><a href="#">Business It</a></li>
+                            <li><a href="#">Office 365</a></li>
+                            <li><a href="#">Consultancy</a></li>
+                            <li><a href="#">Cloud Provider</a></li>
+                            <li><a href="#">Data Backup</a></li>
+                        </ul>
+                    </li>
+                    
+                    <li class="telecoms-box drop-box nav-item">
+                        <a class="telecoms-btn drop-btn" href="#">
+                            <div class="icon"><i class="fas fa-phone-alt"></i></div>
+                            <div class="small">Telecoms</div>
+                            Services
+                        </a>
+                        <span class="back-bar telecoms-bar"></span>
+                        <ul class="telecoms-menu drop-menu">
+                            <li><a href="#">Gigabit Voucher</a></li>
+                            <li><a href="#">Hosted Voip</a></li>
+                            <li><a href="#">Business Voip</a></li>
+                            <li><a href="#">Business Broadband</a></li>
+                            <li><a href="#">Leased Line</a></li>
+                            <li><a href="#">3CX Systems</a></li>
+                        </ul>
+                    </li>
+                    
+                    <li class="software-box drop-box nav-item">
+                        <a class="software-btn drop-btn" href="#">
+                            <div class="icon"><i class="fas fa-th"></i></div>
+                            <div class="small">Bespoke</div>
+                            Software
+                        </a>
+                        <span class="back-bar software-bar"></span>
+                        <ul class="software-menu drop-menu">
+                            <li><a href="#">Workflow</a></li>
+                            <li><a href="#">Automation</a></li>
+                            <li><a href="#">Integration</a></li>
+                            <li><a href="#">Database</a></li>
+                            <li><a href="#">Sharepoint</a></li>
+                            <li><a href="#">Management</a></li>
+                            <li><a href="#">Navision</a></li>
+                        </ul>
+                    </li>
+                    
+                    <li class="marketing-box drop-box nav-item">
+                        <a class="marketing-btn drop-btn" href="#">
+                            <div class="icon"><i class="bi bi-bar-chart-fill"></i></div>
+                            <div class="small">Digital</div>
+                            Marketing
+                        </a>
+                        <span class="back-bar marketing-bar"></span>
+                        <ul class="marketing-menu drop-menu">
+                            <li><a href="#">Search (SEO)</a></li>
+                            <li><a href="#">Paid (PPC)</a></li>
+                            <li><a href="#">Conversion (CRO)</a></li>
+                            <li><a href="#">Email</a></li>
+                            <li><a href="#">Social Media</a></li>
+                            <li><a href="#">Content</a></li>
+                        </ul>
+                    </li>
+                    
+                    <li class="security-box drop-box nav-item">
+                        <a class="security-btn drop-btn" href="#">
+                            <div class="icon"><i class="bi bi-shield-shaded"></i></div>
+                            <div class="small">Cyber</div>
+                            Security
+                        </a>
+                        <span class="back-bar security-bar"></span>
+                        <ul class="security-menu drop-menu">
+                            <li><a href="#">Assessment</a></li>
+                            <li><a href="#">Management</a></li>
+                            <li><a href="#">Penetration Testing</a></li>
+                            <li><a href="#">Cyber Essentials</a></li>
+                            <li><a href="#">PCI/DSS</a></li>
+                            <li><a href="#">Hacker Prevention</a></li>
+                        </ul>
+                    </li>
+                    
+                </ul>
+            </nav>
+        </header>
+        <main>
