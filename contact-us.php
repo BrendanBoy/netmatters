65919e77261fa1225b1497e986ce54288624b0fc
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2022-03-25 14:30:00 +0000

    Add main content sections to the contact-us page

--- contact-us.php
+++ contact-us.php
@@ -1,2 +1,9 @@
 <?php include("inc/header.php"); ?>
+
+
+            <section id="breadcrumb"></section>
+            <section id="office-addresses"></section>
+            <section id="contact-details"></section>
+            <section id="contact-form"></section>
+
 <?php include("inc/footer.php"); ?>
