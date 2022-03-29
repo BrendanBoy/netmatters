18c922d96ff7a894a1f6dca2db11f086f5706928
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2022-03-30 00:10:05 +0100

    Move the News section into its own PHP file

--- index.php
+++ index.php
@@ -209,96 +209,9 @@ include("inc/header.php"); ?>
                     </div>
                 </div>
             </section>
-            <section id="headline">
-                <h2 class="sr-only">News</h2>
-                <div class="container">
-                    <div>
-                        <span>Latest</span>
-                    </div>
-                </div>
-            </section>
-            <section id="news-cards" class="py-4" style="box-shadow: inset 0 5px 35px rgba(0,0,0,0.1);">
-                <div class="container">
-                    <div class="row mb-4">
-                        <div class="col-md-6 col-xl-4 pt-4">
-                            <div class="news-support card h-100">
-                                <div class="image-container">
-                                    <a href="#"><img src="img/news/10-ways-to-22ch.jpg" class="card-img-top" alt=""></a>
-                                    <div class="card-label">News</div>
-                                </div>
-                                <div class="card-body d-flex flex-column">
-                                    <h3 class="card-title">
-                                        <a href="#">10 ways to help make your business more robus...</a>
-                                    </h3>
-                                    <p class="card-text">A robust business is one defined as being able to continue to be
-                                        successful irrespective of the mark...</p>
-                                    <span class="mt-auto">
-                                        <a class="card-link btn btn-support" href="#">Read More</a>
-                                    </span>
-                                </div>
-                                <hr>
-                                <div class="card-footer">
-                                    <img class="float-start rounded-circle" src="img/news/james-gulliver-rgmz.jpg" alt="">
-                                    <p class="author"><strong>Posted by James Gulliver</strong><br>
-                                        <span class="text-muted">5th October 2021</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-xl-4 col pt-4">
-                            <div class="news-design card h-100">
-                                <div class="image-container">
-                                    <a href="#"><img src="img/news/september-2021-notables-qhiw.jpg" class="card-img-top" alt=""></a>
-                                    <div class="card-label">News</div>
-                                </div>
-                                <div class="card-body d-flex flex-column">
-                                    <h3 class="card-title">
-                                        <a href="#">September 2021 Notables</a>
-                                    </h3>
-                                    <p class="card-text">Every month we celebrate the most notable of employees here at
-                                        Netmatters. Each department head is a...</p>
-                                    <span class="mt-auto">
-                                        <a class="card-link btn btn-design" href="#">Read More</a>
-                                    </span>
-                                </div>
-                                <hr>
-                                <div class="card-footer">
-                                    <img class="float-start rounded-circle" src="img/news/netmatters-ltd-VXAv.png" alt="">
-                                    <p class="author"><strong>Posted by Netmatters Ltd</strong><br>
-                                        <span class="text-muted">5th October 2021</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="d-md-none d-xl-block col-xl-4 pt-4">
-                            <!-- This seems a little bit clunky but it works -->
-                            <div class="news-design card h-100">
-                                <div class="image-container">
-                                    <a href="#"><img src="img/news/telemarketing-sales-executive-HapA.jpg" class="card-img-top" alt=""></a>
-                                    <div class="card-label">Careers</div>
-                                </div>
-                                <div class="card-body d-flex flex-column">
-                                    <h3 class="card-title">
-                                        <a href="#">Telemarketing Sales Executive</a>
-                                    </h3>
-                                    <p class="card-text">Salary Range £22,000 (basic) (£30,000 OTE) + Benefits Hours 40
-                                        hours per week, Monday - Friday Locat...</p>
-                                    <span class="mt-auto">
-                                        <a class="card-link btn btn-design" href="#">Read More</a>
-                                    </span>
-                                </div>
-                                <hr>
-                                <div class="card-footer">
-                                    <img class="float-start rounded-circle" src="img/news/mark-reilly-Q6d8.jpg" alt="">
-                                    <p class="author"><strong>Posted by Mark Riley</strong><br>
-                                        <span class="text-muted">28th September 2021</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+
+            <?php include("inc/news.php"); ?>
+
             <section id="clients" style="box-shadow: inset 0 5px 35px rgba(0,0,0,0.1);">
                 <h2 class="sr-only">Clients</h2>
                 <div class="container d-none d-md-block">
