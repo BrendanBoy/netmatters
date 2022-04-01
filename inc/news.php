a4c6926bee53257b2d4202ce51d45500ee1fa35a
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2022-04-01 05:26:36 +0100

    Move the news card foreach loop into the news.php file

--- inc/news.php
+++ inc/news.php
@@ -1,3 +1,28 @@
+<?php
+
+function get_articles() {
+    include("inc/connect.php");
+
+    try {
+        $result = $db->query(
+            "SELECT category_name, service_name, image, title, body, author_image, author_name, date 
+            FROM Articles
+            JOIN Categories ON Articles.category_id = Categories.category_id
+            JOIN Services ON Articles.service_id = Services.service_id
+            JOIN Authors ON Articles.author_id = Authors.author_id"
+        );
+    } catch (Exception $e) {
+        echo "Unable to retrive result";
+        exit;
+    }
+
+    $items = $result->fetchAll(PDO::FETCH_ASSOC);
+    return $items;
+}
+
+$articles = get_articles();
+
+?>
 <!-- start of news section -->
             <section id="headline">
                 <h2 class="sr-only">News</h2>
@@ -10,82 +35,36 @@
             <section id="news-cards" class="py-4" style="box-shadow: inset 0 5px 35px rgba(0,0,0,0.1);">
                 <div class="container">
                     <div class="row mb-4">
-                        <div class="col-md-6 col-xl-4 pt-4">
-                            <div class="news-support card h-100">
+                    
+                    <?php foreach($articles as $article): ?>
+                        
+                        <div class="article col-md-6 col-xl-4 pt-4">
+                            <div class="news-<?php echo $article["service_name"] ?> card h-100">
                                 <div class="image-container">
-                                    <a href="#"><img src="img/news/10-ways-to-22ch.jpg" class="card-img-top" alt=""></a>
-                                    <div class="card-label">News</div>
+                                    <a href="#"><img src="img/news/<?php echo $article["image"] ?>" class="card-img-top" alt=""></a>
+                                    <div class="card-label"><?php echo $article["category_name"] ?></div>
                                 </div>
                                 <div class="card-body d-flex flex-column">
                                     <h3 class="card-title">
-                                        <a href="#">10 ways to help make your business more robus...</a>
+                                        <a href="#"><?php echo $article["title"] ?></a>
                                     </h3>
-                                    <p class="card-text">A robust business is one defined as being able to continue to be
-                                        successful irrespective of the mark...</p>
+                                    <p class="card-text"><?php echo $article["body"] ?></p>
                                     <span class="mt-auto">
-                                        <a class="card-link btn btn-support" href="#">Read More</a>
+                                        <a class="card-link btn btn-<?php echo $article["service_name"] ?>" href="#">Read More</a>
                                     </span>
                                 </div>
                                 <hr>
                                 <div class="card-footer">
-                                    <img class="float-start rounded-circle" src="img/news/james-gulliver-rgmz.jpg" alt="">
-                                    <p class="author"><strong>Posted by James Gulliver</strong><br>
-                                        <span class="text-muted">5th October 2021</span>
+                                    <img class="float-start rounded-circle" src="img/news/<?php echo $article["author_image"] ?>" alt="">
+                                    <p class="author"><strong>Posted by <?php echo $article["author_name"] ?></strong><br>
+                                        <span class="text-muted"><?php echo date_format(date_create($article["date"]),"jS F Y"); ?></span>
                                     </p>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-6 col-xl-4 col pt-4">
-                            <div class="news-design card h-100">
-                                <div class="image-container">
-                                    <a href="#"><img src="img/news/september-2021-notables-qhiw.jpg" class="card-img-top" alt=""></a>
-                                    <div class="card-label">News</div>
-                                </div>
-                                <div class="card-body d-flex flex-column">
-                                    <h3 class="card-title">
-                                        <a href="#">September 2021 Notables</a>
-                                    </h3>
-                                    <p class="card-text">Every month we celebrate the most notable of employees here at
-                                        Netmatters. Each department head is a...</p>
-                                    <span class="mt-auto">
-                                        <a class="card-link btn btn-design" href="#">Read More</a>
-                                    </span>
-                                </div>
-                                <hr>
-                                <div class="card-footer">
-                                    <img class="float-start rounded-circle" src="img/news/netmatters-ltd-VXAv.png" alt="">
-                                    <p class="author"><strong>Posted by Netmatters Ltd</strong><br>
-                                        <span class="text-muted">5th October 2021</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="d-md-none d-xl-block col-xl-4 pt-4">
-                            <!-- This seems a little bit clunky but it works -->
-                            <div class="news-design card h-100">
-                                <div class="image-container">
-                                    <a href="#"><img src="img/news/telemarketing-sales-executive-HapA.jpg" class="card-img-top" alt=""></a>
-                                    <div class="card-label">Careers</div>
-                                </div>
-                                <div class="card-body d-flex flex-column">
-                                    <h3 class="card-title">
-                                        <a href="#">Telemarketing Sales Executive</a>
-                                    </h3>
-                                    <p class="card-text">Salary Range £22,000 (basic) (£30,000 OTE) + Benefits Hours 40
-                                        hours per week, Monday - Friday Locat...</p>
-                                    <span class="mt-auto">
-                                        <a class="card-link btn btn-design" href="#">Read More</a>
-                                    </span>
-                                </div>
-                                <hr>
-                                <div class="card-footer">
-                                    <img class="float-start rounded-circle" src="img/news/mark-reilly-Q6d8.jpg" alt="">
-                                    <p class="author"><strong>Posted by Mark Riley</strong><br>
-                                        <span class="text-muted">28th September 2021</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+
+                    <?php endforeach; ?>
+
                     </div>
                 </div>
             </section>
