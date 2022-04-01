e7aa8ad878c86c4295ccc4a5a573d5c9811a8d48
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2022-04-01 04:21:56 +0100

    Create and test foreach loop for populating the news cards

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 include('inc/header.php');
 
-function get_article() {
+function get_articles() {
     include("inc/connect.php");
 
     try {
@@ -10,23 +10,24 @@ function get_article() {
             FROM Articles
             JOIN Categories ON Articles.category_id = Categories.category_id
             JOIN Services ON Articles.service_id = Services.service_id
-            JOIN Authors ON Articles.author_id = Authors.author_id
-            WHERE Articles.id = 3"
+            JOIN Authors ON Articles.author_id = Authors.author_id"
         );
     } catch (Exception $e) {
         echo "Unable to retrive result";
         exit;
     }
 
-    $item = $result->fetch(PDO::FETCH_ASSOC);
-    return $item;
+    $items = $result->fetchAll(PDO::FETCH_ASSOC);
+    return $items;
 }
 
-$article = get_article();
+$articles = get_articles();
 
 ?>
  
 <div class="row mb-4 justify-content-center">
+
+    <?php foreach($articles as $article): ?>
     <div class="col-md-6 col-xl-4 pt-4">
         <div class="news-<?php echo $article["service_name"] ?> card h-100">
             <div class="image-container">
@@ -51,31 +52,8 @@ $article = get_article();
             </div>
         </div>
     </div>
-    <div class="col-md-6 col-xl-4 pt-4">
-        <div class="news-support card h-100">
-            <div class="image-container">
-                <a href="#"><img src="img/news/10-ways-to-22ch.jpg" class="card-img-top" alt=""></a>
-                <div class="card-label">News</div>
-            </div>
-            <div class="card-body d-flex flex-column">
-                <h3 class="card-title">
-                    <a href="#">10 ways to help make your business more robus...</a>
-                </h3>
-                <p class="card-text">A robust business is one defined as being able to continue to be
-                    successful irrespective of the mark...</p>
-                <span class="mt-auto">
-                    <a class="card-link btn btn-support" href="#">Read More</a>
-                </span>
-            </div>
-            <hr>
-            <div class="card-footer">
-                <img class="float-start rounded-circle" src="img/news/james-gulliver-rgmz.jpg" alt="">
-                <p class="author"><strong>Posted by James Gulliver</strong><br>
-                    <span class="text-muted">5th October 2021</span>
-                </p>
-            </div>
-        </div>
-    </div>
+    <?php endforeach; ?>
+    
 </div>
 
 <?php include("inc/footer.php") ?>
