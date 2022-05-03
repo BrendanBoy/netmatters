fc7f73ac3a58ae95e123183a9d9df9d85074dc19
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2022-05-03 12:11:22 +0100

    Animate the show/hide transition for the out of hours section

--- js/out-of-hours.js
+++ js/out-of-hours.js
@@ -1,9 +1,5 @@
 const oohDetails = document.getElementById("out-of-hours");
 
 function ooh() {
-    if (oohDetails.style.display == "block") {
-        oohDetails.style.display = "none";
-    } else {
-        oohDetails.style.display = "block";
-    }
+    oohDetails.classList.toggle("is-visible");
 }
